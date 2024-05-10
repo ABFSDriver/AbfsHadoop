93e355941c19909ddd02eb9fd3fb386009948902
Anmol Asrani <anmol.asrani123@gmail.com>
2024-05-10 05:29:50 -0700

    Make threads configurable

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/WriteThreadPoolSizeManager.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/WriteThreadPoolSizeManager.java
@@ -4,37 +4,39 @@ import org.apache.hadoop.fs.azurebfs.services.AbfsOutputStream;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
-import static org.apache.hadoop.fs.azurebfs.constants.ConfigurationKeys.FS_AZURE_BLOCK_UPLOAD_ACTIVE_BLOCKS;
-import static org.apache.hadoop.fs.azurebfs.constants.FileSystemConfigurations.BLOCK_UPLOAD_ACTIVE_BLOCKS_DEFAULT;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class WriteThreadPoolSizeManager {
     private static WriteThreadPoolSizeManager instance;
     private final int maxPoolSize;
-    private final List<AbfsOutputStream> outputStreams;
+    private final Set<AbfsOutputStream> outputStreams;
     private final ScheduledExecutorService cpuMonitorExecutor;
     private final ExecutorService boundedThreadPool;
     private final double cpuThreshold = 0.50;
     private int concurrentWrites;
+    private final Lock lock = new ReentrantLock();
+    private AtomicInteger pendingTasks;
 
     private WriteThreadPoolSizeManager(AbfsConfiguration abfsConfiguration) {
-        concurrentWrites = abfsConfiguration.getBlockOutputActiveBlocks();
+        concurrentWrites = (abfsConfiguration.getBlockOutputActiveBlocks());
         if (concurrentWrites < 1) {
             concurrentWrites = 1;
         }
         maxPoolSize = Math.min(60 * concurrentWrites, 6 * Runtime.getRuntime().availableProcessors());
-        outputStreams = Collections.synchronizedList(new ArrayList<>());
+        outputStreams = Collections.newSetFromMap(new ConcurrentHashMap<>());
         boundedThreadPool = Executors.newFixedThreadPool(maxPoolSize);
         cpuMonitorExecutor = Executors.newScheduledThreadPool(1);
-        startCPUMonitoring();
+        pendingTasks = new AtomicInteger(0);
     }
 
     public static synchronized WriteThreadPoolSizeManager getInstance(AbfsConfiguration abfsConfiguration) {
@@ -44,36 +46,42 @@ public class WriteThreadPoolSizeManager {
         return instance;
     }
 
-    private synchronized void adjustThreadPoolSizeBasedOnOutputStreams() throws InterruptedException {
-        int currentThreadPoolSize = ((ThreadPoolExecutor) boundedThreadPool).getMaximumPoolSize();
-        int currentOutputStreams = getTotalOutputStreams();
-        int newMaxPoolSize = Math.min(10 * concurrentWrites, currentThreadPoolSize + (currentOutputStreams * concurrentWrites));
-        adjustThreadPoolSize(newMaxPoolSize);
-    }
-
-    public synchronized void adjustThreadPoolSize(int newMaxPoolSize) throws InterruptedException {
-        ThreadPoolExecutor threadPoolExecutor = ((ThreadPoolExecutor) boundedThreadPool);
-        newMaxPoolSize = Math.max(threadPoolExecutor.getCorePoolSize(), newMaxPoolSize);
-        System.out.println("The thread pool size is: " + newMaxPoolSize);
-        threadPoolExecutor.setMaximumPoolSize(newMaxPoolSize);
+    public void adjustThreadPoolSize(int newMaxPoolSize) throws InterruptedException {
+        synchronized (this) {
+            ThreadPoolExecutor threadPoolExecutor = ((ThreadPoolExecutor) boundedThreadPool);
+            int currentCorePoolSize = threadPoolExecutor.getCorePoolSize();
+            if (newMaxPoolSize > currentCorePoolSize) {
+                threadPoolExecutor.setMaximumPoolSize(newMaxPoolSize);
+                int tasksToSubmit = newMaxPoolSize - currentCorePoolSize;
+                for (int i = 0; i < tasksToSubmit; i++) {
+                    submitPlaceholderTask();
+                }
+            }
+            System.out.println("The thread pool size is: " + newMaxPoolSize);
+        }
         notifyAbfsOutputStreams(newMaxPoolSize);
     }
 
     public void registerAbfsOutputStream(AbfsOutputStream outputStream) {
-        outputStreams.add(outputStream);
-        try {
-            adjustThreadPoolSizeBasedOnOutputStreams();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        if (!outputStreams.contains(outputStream)) {
+            outputStreams.add(outputStream);
+            try {
+                System.out.println("The number of outputstreams is " + getTotalOutputStreams());
+                adjustThreadPoolSizeBasedOnOutputStreams();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
     public void deRegisterAbfsOutputStream(AbfsOutputStream outputStream) {
-        outputStreams.remove(outputStream);
-        try {
-            adjustThreadPoolSizeBasedOnOutputStreams();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        if (outputStreams.contains(outputStream)) {
+            outputStreams.remove(outputStream);
+            try {
+                adjustThreadPoolSizeBasedOnOutputStreams();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
@@ -81,7 +89,7 @@ public class WriteThreadPoolSizeManager {
         return outputStreams.size();
     }
 
-    private synchronized void notifyAbfsOutputStreams(int newPoolSize) throws InterruptedException {
+    private void notifyAbfsOutputStreams(int newPoolSize) throws InterruptedException {
         for (AbfsOutputStream outputStream : outputStreams) {
             outputStream.poolSizeChanged(newPoolSize);
         }
@@ -91,7 +99,14 @@ public class WriteThreadPoolSizeManager {
         return maxPoolSize;
     }
 
-    public synchronized void startCPUMonitoring() {
+    private void submitPlaceholderTask() {
+        pendingTasks.incrementAndGet();
+        boundedThreadPool.submit(() -> {
+            pendingTasks.decrementAndGet();
+        });
+    }
+
+    public void startCPUMonitoring() {
         cpuMonitorExecutor.scheduleAtFixedRate(() -> {
             double cpuUtilization = getCpuUtilization();
             System.out.println("Current CPU Utilization is this: " + cpuUtilization);
@@ -115,15 +130,35 @@ public class WriteThreadPoolSizeManager {
         return 0.0;
     }
 
-    private synchronized void adjustThreadPoolSizeBasedOnCPU(double cpuUtilization) throws InterruptedException {
+    public void adjustThreadPoolSizeBasedOnCPU(double cpuUtilization) throws InterruptedException {
         int newMaxPoolSize;
-        int currentPoolSize = ((ThreadPoolExecutor) boundedThreadPool).getMaximumPoolSize();
-        if (cpuUtilization > cpuThreshold) {
-            newMaxPoolSize = Math.max(1, currentPoolSize / 2);
-        } else {
-            newMaxPoolSize = Math.min(2 * currentPoolSize, Math.max(maxPoolSize, 10 * concurrentWrites));
+        lock.lock();
+        try {
+            int currentPoolSize = ((ThreadPoolExecutor) boundedThreadPool).getMaximumPoolSize();
+            if (cpuUtilization > cpuThreshold) {
+                newMaxPoolSize = Math.max(1, currentPoolSize / 2);
+            } else {
+                newMaxPoolSize = Math.min(2 * currentPoolSize, Math.max(maxPoolSize, 55 * concurrentWrites));
+            }
+            System.out.println("The new max pool size: " + newMaxPoolSize);
+        } finally {
+            lock.unlock();
         }
-        System.out.println("The new max pool size: " + newMaxPoolSize);
+        adjustThreadPoolSize(newMaxPoolSize);
+    }
+
+    public void adjustThreadPoolSizeBasedOnOutputStreams() throws InterruptedException {
+        int newMaxPoolSize;
+        lock.lock();
+        try {
+            int currentThreadPoolSize = ((ThreadPoolExecutor) boundedThreadPool).getMaximumPoolSize();
+            int currentOutputStreams = outputStreams.size();
+            int potentialSize = Math.max(currentOutputStreams * concurrentWrites, currentThreadPoolSize);
+            System.out.println("The potential size is " + potentialSize);
+            newMaxPoolSize = Math.min(55 * concurrentWrites, potentialSize);
+        } finally {
+            lock.unlock();
+        }
         adjustThreadPoolSize(newMaxPoolSize);
     }
 
