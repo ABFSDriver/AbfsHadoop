2ca1b0e299b61cdc241d6e1e27de70fdf12a8ce0
Anuj Modi <anujmodi@microsoft.com>
2024-07-01 05:02:31 -0700

    Azcopy helper Test

---
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/utils/TestAzcopyToolHelper.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.fs.azurebfs.utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.azurebfs.AbstractAbfsIntegrationTest;
+import org.apache.hadoop.fs.azurebfs.AzureBlobFileSystem;
+
+public class TestAzcopyToolHelper extends AbstractAbfsIntegrationTest {
+
+  public TestAzcopyToolHelper() throws Exception {
+
+  }
+
+  @Test
+  public void testGetAzcopyToolCommand() throws Exception {
+    AzureBlobFileSystem fs = this.getFileSystem();
+    Path filePath = path("dir1/file.txt");
+    Path folderPath = path("dir2");
+    this.createAzCopyFile(filePath);
+    this.createAzCopyFolder(folderPath);
+
+    Assertions.assertThat(DirectoryStateHelper.isImplicitDirectory(
+        filePath.getParent(), fs, getTestTracingContext(fs, false)))
+        .describedAs("File created by azcopy should have implicit parent")
+        .isTrue();
+    Assertions.assertThat(DirectoryStateHelper.isImplicitDirectory(
+        folderPath, fs, getTestTracingContext(fs, false)))
+        .describedAs("Folder created by azcopy should be implicit")
+        .isTrue();
+    Assertions.assertThat(fs.getFileStatus(filePath).isFile())
+        .describedAs("File created by azcopy should exist")
+        .isTrue();
+  }
+}
